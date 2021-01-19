42ab47db1db2a4cf1f3fec2e4d7e3c4bf5c0ef3a
Jacob Thompson <Cobalt7291@gmail.com>
2021-01-18 21:27:17 -0500

    added migrations to fix price, description, and added inventory

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+  #place customized methods to format like ingredients list for recipe app
+  #anything relating to data goes here
+  #database powers the model
+  def is_discounted?
+    if price < 10
+      true
+    else
+      false
+    end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_015639) do
+ActiveRecord::Schema.define(version: 2021_01_19_021540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 5, scale: 2
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "inventory"
   end
 
 end
